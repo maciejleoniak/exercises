83ef3a531e3effc9a54bf1b509bcfe5214357bcf
7799043 <leoniak.maciek1@gmail.com>
2023-05-12 13:16:18 +0200

    done

--- 1.js
+++ 1.js
@@ -1,1 +1,19 @@
 /*  Count duplicate characters in the string. Output can be a HashMap<String, Integer> or whatever container you like)  */ 
+
+function countDuplicateChars(str) { 
+    const charCount = {}; 
+    for (let char of str) { 
+      charCount[char] = (charCount[char] || 0) + 1; 
+    }
+    const duplicates = {};
+    for (let char in charCount) {
+      if (charCount[char] > 1) {
+        duplicates[char] = charCount[char];
+      }
+    }
+    return duplicates;
+  }
+
+const str = "programming";
+const duplicates = countDuplicateChars(str);
+console.log(duplicates);
