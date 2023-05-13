856ce632f6081686ec870d7546ef53cfa8eca336
7799043 <leoniak.maciek1@gmail.com>
2023-05-13 14:10:46 +0200

    almost

--- 2.js
+++ 2.js
@@ -1,1 +1,24 @@
 /* Find the 1st non-repeated character in the string */
+
+
+function findFirstNonRepeatedChar (str) {
+    const charCount = {};
+    for (let char of str){
+        charCount[char] = ( charCount[char] || 0)+1;
+    }
+    const showSingle = {};
+    for (let char in charCount) {
+    if (charCount[char] <= 1) {
+    showSingle[char] = charCount[char];
+    
+  }
+}
+return showSingle;
+}
+
+
+const str = "kaboom";
+const showSingle = findFirstNonRepeatedChar(str);
+console.log(showSingle);
+
+
