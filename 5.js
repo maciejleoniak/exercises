5b962755e9c9473365532d96ef273b373319bbee
7799043 <leoniak.maciek1@gmail.com>
2023-05-15 12:42:09 +0200

    done

--- 5.js
+++ 5.js
@@ -1,5 +1,13 @@
 /* Count the number of vowels and consonants in the string */
 
-function countNumOfVowelsAndConsonants (string){
-    
-};
+    function countNumOfVowels(str){
+        const vowels = str.match(/[auioe]/gi);
+        return vowels.length; 
+    }
+    function countNumOfConsonants(str){
+        const consonants = str.match(/[a-z]/gi);
+        return consonants.length; 
+    }
+
+const string1 = "nice hacky";
+console.log("number of vovels:", countNumOfVowels(string1), "number of consonants:", countNumOfConsonants(string1)-countNumOfVowels(string1));
