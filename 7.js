e2349e294ac72a95b4de403b118355c37f60b6cc
7799043 <leoniak.maciek1@gmail.com>
2023-05-16 11:41:16 +0200

    .

--- 7.js
+++ 7.js
@@ -2,4 +2,24 @@
 Generate all permutations for the string
 */
 
-function generateAllPermutations (str){};
+function generateAllPermutations (str){
+};
+   
+   
+
+
+
+function silnia(n) {
+        if (n < 0) {
+          return NaN;
+        }
+        let factorial = 1;
+        for (let i = 2; i <= n; i++) {
+          factorial *= i;
+        }
+        return factorial;
+};
+
+const n = 3;
+const factorial = silnia(n);
+console.log(factorial);
