6425f7598107736c97a20b73a0286a8193c2aa72
7799043 <leoniak.maciek1@gmail.com>
2023-05-17 19:08:36 +0200

    done

--- 7.js
+++ 7.js
@@ -2,24 +2,25 @@
 Generate all permutations for the string
 */
 
-function generateAllPermutations (str){
+function generateAllPermutations(str) {
+  const permutations = [];
+
+  function permute(prefix, remaining) {
+    if (remaining.length === 0) {
+      permutations.push(prefix);
+    } else {
+      for (let i = 0; i < remaining.length; i++) {
+        const newPrefix = prefix + remaining[i];
+        const newRemaining = remaining.slice(0, i) + remaining.slice(i + 1);
+        permute(newPrefix, newRemaining);
+      };
+    };
+  };
+
+  permute('', str);
+  return permutations;
 };
-   
-   
 
-
-
-function silnia(n) {
-        if (n < 0) {
-          return NaN;
-        }
-        let factorial = 1;
-        for (let i = 2; i <= n; i++) {
-          factorial *= i;
-        }
-        return factorial;
-};
-
-const n = 3;
-const factorial = silnia(n);
-console.log(factorial);
+const inputString = 'ups';
+const result = generateAllPermutations(inputString);
+console.log(result);
