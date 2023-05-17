f5d66e2cc0cfc41b379e9f6634ee4ebd3b659890
7799043 <leoniak.maciek1@gmail.com>
2023-05-17 19:40:49 +0200

    done

--- 8.js
+++ 8.js
@@ -4,8 +4,9 @@ Check if the word is a palindrome
 
 function isAPalindrome(str) {
     str = str.toLowerCase();
+    str = str.replace(/[^a-zA-Z0-9]/g, "");
     const reversed = str.split("").reverse().join("");
-    if (str == reversed){
+    if (str === reversed){
         return "It's a palindrome.";
     }else{
         return "It's not a palindrome.";
