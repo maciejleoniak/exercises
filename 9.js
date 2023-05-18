ff1f6913540be0a847d29faeccb2837d49ef03db
7799043 <leoniak.maciek1@gmail.com>
2023-05-18 18:05:07 +0200

    done

--- 9.js
+++ 9.js
@@ -2,3 +2,15 @@
 Remove duplicate characters from the string
 */
 
+function removeDuplicateCharacters(str) {
+    const characters = str.split("");
+    const singleChar = characters.filter((char, index) => {
+      return characters.indexOf(char) === index;
+    });
+    const result = singleChar.join("");
+    return result;
+  };
+  
+  const str = "This is okey?";
+  const stringWithoutDuplicates = removeDuplicateCharacters(str);
+  console.log(stringWithoutDuplicates);
