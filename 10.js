622dcb6d809b9ebce4237961111997f6a5b1f929
7799043 <leoniak.maciek1@gmail.com>
2023-05-18 18:22:46 +0200

    done

--- 10.js
+++ 10.js
@@ -2,3 +2,22 @@
 Remove a given character from the string
 */
 
+function sentence(str) {
+    return str.split('');
+  }
+  
+  function deleteChar(array, char) {
+    return array.filter(function(element) {
+        return element !== char;
+      });
+  };
+
+  const inputString = 'abba';
+  const charToDelete = 'b';
+  
+  const splited = sentence(inputString);
+  const toJoin = deleteChar(splited, charToDelete);
+  
+  const sentenceWithoutChar = toJoin.join('');
+  
+  console.log(sentenceWithoutChar);
