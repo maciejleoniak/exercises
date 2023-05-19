51bd98fc71045256e2c30d4b3258d9c9cbeb3787
7799043 <leoniak.maciek1@gmail.com>
2023-05-19 11:15:57 +0200

    done

--- 12.js
+++ 12.js
@@ -1,3 +1,30 @@
 /*
 Find the longest common prefix for an array of strings 
 */
+
+function longestCommonPrefix(strings) {
+    if (strings.length === 0) {
+      return '';
+    };
+  
+    let prefix = '';
+    const firstString = strings[0];
+  
+    for (let i = 0; i < firstString.length; i++) {
+      const char = firstString[i];
+  
+      for (let j = 1; j < strings.length; j++) {
+        if (strings[j][i] !== char) {
+          return prefix;
+        };
+      };
+  
+      prefix += char;
+    };
+  
+    return prefix;
+  };
+  
+  const strings = ['home', 'homeland', 'homless'];
+  const commonPrefix = longestCommonPrefix(strings);
+  console.log(commonPrefix);
