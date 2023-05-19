de221d1419c3cd5924720869577708355ba129ff
7799043 <leoniak.maciek1@gmail.com>
2023-05-19 11:00:34 +0200

    done

--- 11.js
+++ 11.js
@@ -2,3 +2,25 @@
 Find the characters with the most appearances
 */
 
+function characterWithTheMostApperances(str) { 
+    const charCount = {}; 
+    let maxCount=0;
+    for (let char of str) { /* (let char of str){... = (let i = 0; i < str.length; i++) {
+        const char = str[i]; ... */ 
+      charCount[char] = (charCount[char] || 0) + 1; 
+      if (charCount[char]>maxCount){
+        maxCount = charCount[char];
+      };
+    };
+    const charMaxCount = [];
+    for (let char in charCount) {
+      if (charCount[char] === maxCount) {
+        charMaxCount.push(char);
+      };
+    };
+    return charMaxCount;
+  };
+
+const str = "Characters with the most appearances";
+const theMostApperances = characterWithTheMostApperances(str);
+console.log(theMostApperances);
