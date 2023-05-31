b57eeae5db3f7a48b15e10bbc8187bdb5c022743
7799043 <leoniak.maciek1@gmail.com>
2023-05-31 10:38:38 +0200

    corrected code

--- 02.js
+++ 02.js
@@ -1,23 +1,22 @@
 /* Find the 1st non-repeated character in the string */
 
 
-function findFirstNonRepeatedChar (str) {
-    const charCount = {};
-    for (let char of str){
-        charCount[char] = ( charCount[char] || 0)+1;
-    }
-    const showSingle = {};
-    for (let char in charCount) {
-    if (charCount[char] <= 1) {
-    showSingle[char] = charCount[char];
+function findFirstNonRepeatedChar(str) {
+  const charCount = {};
+  for (let char of str) {
+    charCount[char] = (charCount[char] || 0) + 1;
   }
-}
-return showSingle;
+  const showSingle = {};
+  for (let char in charCount) {
+    if (charCount[char] === 1) {
+      showSingle[char] = charCount[char];
+    }
+  }
+  const showFirst = Object.entries(showSingle)[0];
+  return showFirst ? showFirst[0] : null;
 }
 
-const str = "kaboom";
-const showSingle = findFirstNonRepeatedChar(str);
-const showFirst = Object.entries(showSingle)[0];
-console.log(showFirst); 
+const str = "kkaboom";
+console.log(findFirstNonRepeatedChar(str));
 
 
