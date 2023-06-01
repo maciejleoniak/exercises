5e0a2d90106fba255da1aa8cfb2f74cc78b8a723
7799043 <leoniak.maciek1@gmail.com>
2023-06-01 09:31:04 +0200

    done

--- test02.js
+++ test02.js
@@ -28,5 +28,13 @@ function testfindFirstNonRepeatedChar() {
     console.log("Test 1 - Expected:", expected1);
     console.log("Test 1 - Result:", duplicates1);
     console.log("Test 1 - Pass:", JSON.stringify(duplicates1) === JSON.stringify(expected1));
+
+    const str2 = "aaa";
+    const expected2 = null;
+    const result2 = findFirstNonRepeatedChar(str2);
+    console.log("Test 2 - Expected:", expected2);
+    console.log("Test 2 - Result:", result2);
+    console.log("Test 2 - Pass:", result2 === expected2);
+
 }
 testfindFirstNonRepeatedChar()
