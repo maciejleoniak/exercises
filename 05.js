074038707b77aaae4536a8005929c3637b6e39fe
7799043 <leoniak.maciek1@gmail.com>
2023-06-02 09:41:45 +0200

    corrected code

--- 05.js
+++ 05.js
@@ -1,13 +1,17 @@
-/* Count the number of vowels and consonants in the string */
-
-    function countNumOfVowels(str){
-        const vowels = str.match(/[auioe]/gi);
-        return vowels.length; 
-    }
-    function countNumOfConsonants(str){
-        const consonants = str.match(/[a-z]/gi);
-        return consonants.length; 
-    }
-
-const string1 = "nice hacky";
-console.log("number of vovels:", countNumOfVowels(string1), "number of consonants:", countNumOfConsonants(string1)-countNumOfVowels(string1));
+function countNumOfVowels(str) {
+    const vowels = str.match(/[aeiou]/gi);
+    return vowels ? vowels.length : 0;
+  }
+  
+  function countNumOfConsonants(str) {
+    const consonants = str.match(/[bcdfghjklmnpqrstvwxyz]/gi);
+    return consonants ? consonants.length : 0;
+  }
+  
+  const string1 = "nice hacky";
+  console.log(
+    "Number of vowels:",
+    countNumOfVowels(string1),
+    "Number of consonants:",
+    countNumOfConsonants(string1)
+  );
