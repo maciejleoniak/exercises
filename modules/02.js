c16fd5056e13185118cc0f7a41cb0dcac6401450
7799043 <leoniak.maciek1@gmail.com>
2023-06-05 21:26:16 +0200

    .

--- modules/02.js
+++ modules/02.js
@@ -16,7 +16,7 @@ function findFirstNonRepeatedChar(str) {
   return showFirst ? showFirst[0] : null;
 }
 
-const str = "kkaboom";
-console.log(findFirstNonRepeatedChar(str));
+// const str = "kkaboom";
+// console.log(findFirstNonRepeatedChar(str));
 
 module.exports = findFirstNonRepeatedChar;
